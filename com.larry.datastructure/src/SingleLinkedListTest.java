f7c255f3c727b64e37e053bb8fb2df4f5daa5d70
larrywang <111@qq.com>
2025-03-04 00:02:38 +0800

    singleLinkedList fifth commit!

--- com.larry.datastructure/src/SingleLinkedListTest.java
+++ com.larry.datastructure/src/SingleLinkedListTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 
-public class test {
+public class SingleLinkedListTest {
     @Test
     @DisplayName("loop!")
     public void test1() {
